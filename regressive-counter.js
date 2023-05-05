3215552c21284e5ff1865de167a556b93b2e7112
Thales Davi de Souza <81756816+ThalesDaviSouza@users.noreply.github.com>
2023-05-05 17:11:31 -0300

    Primeiros testes de fazer o countdown

--- regressive-counter.js
+++ regressive-counter.js
@@ -1,3 +1,8 @@
+const second = 1000;
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24 
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.regressive-counter').forEach(counter => {
         let deadlineData = {
@@ -8,13 +13,28 @@ document.addEventListener("DOMContentLoaded", () => {
             minute: counter.dataset.deadlineMinute,
             second: counter.dataset.deadlineSecond
         }
+
         let deadlineDate = new Date(deadlineData.year, deadlineData.mounth, deadlineData.day, deadlineData.Hour, deadlineData.minute, deadlineData.second)
         
         let actualDate = new Date()
-        let test = deadlineDate.getTime() - actualDate.getTime()
+
         
-        console.log(test)
+        let stillTime = (deadlineDate.getTime() - actualDate.getTime()) > 0
+
+        if(stillTime){
+            setDayRemaining(counter, stillTime)
+        }
+
         console.log(counter.querySelector('.day'))
 
     })
 })
+
+function millisecondsToDays(milliseconds){
+    return Math.floor((milliseconds % day))
+}
+
+function setDayRemaining(counter, stillTime){
+    console.log(millisecondsToDays(stillTime))
+    counter.querySelector('.day').innerHtml = `${millisecondsToDays(stillTime)}`  
+}
